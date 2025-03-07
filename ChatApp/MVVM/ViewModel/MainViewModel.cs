672a579f2737cc0d4bb521ba70ca61d74c7fd3c8
opilane <opilane@HP-CZC1339D21>
2025-03-07 11:02:10 +0200

    Add project files.

---
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ChatApp.MVVM.Core;
+using ChatApp.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        private Server _server;
+
+
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
+        }
+        
+    }
+}
